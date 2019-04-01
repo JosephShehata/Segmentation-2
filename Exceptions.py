70b56873f446ecfdca3530b2cb6583725482b548
JosephShehata <29582074+JosephShehata@users.noreply.github.com>
2019-04-01 20:35:20 +0100

    Update Exceptions.py

--- Exceptions.py
+++ Exceptions.py
@@ -2,10 +2,7 @@ __all__ = ['BadSignalWarning']
 
 class BadSignalWarning(UserWarning):
     '''
-    warning class to raise when no heart rate is detectable
-    in supplied signal.
+    Warning class to raise when no heart rate is detectable in supplied signal.
     
-    This warning notifies the user that the supplied signal is
-    of insufficient quality and/or does not contain enough information
-    to properly process.
+    This warning notifies the user that the supplied signal is of insufficient quality and/or does not contain enough information to properly process.
     '''
